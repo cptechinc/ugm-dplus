c5ed74c71a0bdd14a203733e89ade32cfd210d04
Paul <pauldro952@outlook.com>
2021-06-28 14:06:20 -0500

    FIX: handle apostrophes with attrJS

--- site/templates/twig/util/functions.php
+++ site/templates/twig/util/functions.php
@@ -40,8 +40,8 @@
 	$config->twig->addFilter($filter);
 
 	$filter = new Twig_Filter('attrJS', function ($string, $jsprepend = true) {
-		$replace = [' ', '&', '+'];
-		$replacewith = ['-', 'amp', 'plus'];
+		$replace = [' ', '&', '+', "'"];
+		$replacewith = ['-', 'amp', 'plus', 'apos'];
 		$string = str_replace($replace, $replacewith, $string);
 		return $jsprepend ? "js-$string" : $string;
 	});
